3d64c6243ef4a2e31d5ea0f649669363a0b50db5
M.Gogolin <m4xin4t4@gmail.com>
2024-08-06 12:54:07 +0500

    Fourth commit.

--- src/main/java/com/gridnine/testing/Main.java
+++ src/main/java/com/gridnine/testing/Main.java
@@ -6,52 +6,39 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println("Неотфильтрованные полеты:");
         List<Flight> flights = FlightBuilder.createFlights();
         flights.forEach(System.out::println);
 
-        Filter filter = new Filter();
-
-        filter.addFilter(new ArrivalBeforeDepartureFlightFilter());
-
-        List<Flight> filteredFlights = filter.filterFlights(flights);
-
+        //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
         System.out.println("\nПолеты отфильтрованные от полетов, где дата и время прибытия раньше чем дата и время вылета:");
-
+        Filter filter = new Filter();
+        filter.addFilter(new ArrivalBeforeDepartureFlightFilter());
+        List<Flight> filteredFlights = filter.filterFlights(flights);
         filteredFlights.forEach(System.out::println);
 
         //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        System.out.println("\nПолеты отфильтрованные от полетов, где дата и время раньше текущих даты и времени:");
         filter.clear();
         filter.addFilter(new TimeDepartureFlightFilter());
-
-        System.out.println("\nПолеты отфильтрованные от полетов, где дата и время раньше текущих даты и времени:");
-
         List<Flight> currentDateAndTimeFlights = filter.filterFlights(flights);
-
         currentDateAndTimeFlights.forEach(System.out::println);
 
         //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
         System.out.println("\nПолеты отфильтрованные от полетов, где общее время нахождения на земле больше 2 часов:");
-
         filter.clear();
         filter.addFilter(new GroundTimeFlightFilter()); // 2 hours in minutes
-
         List<Flight> groundTimeFlightsMoreTwoHours = filter.filterFlights(flights);
-
         groundTimeFlightsMoreTwoHours.forEach(System.out::println);
 
         //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
         System.out.println("\nПрименение всех фильтров сразу.");
-
+        filter.clear();
         List<FlightFilter> filters = List.of(new ArrivalBeforeDepartureFlightFilter(),
                 new TimeDepartureFlightFilter(),
                 new GroundTimeFlightFilter());
-
-        filter.clear();
         filter.addFilters(filters);
-
         List<Flight> allFiltersFlights = filter.filterFlights(flights);
-
         allFiltersFlights.forEach(System.out::println);
     }
 
